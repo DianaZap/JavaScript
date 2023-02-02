f5f3005618f708a4384dd19637f804fc4a15d38e
zasa87 <zasa.arte@gmail.com>
2023-02-02 00:12:08 -0500

    Simulador interactivo intento 1

---
+++ script.js
@@ -0,0 +1,75 @@
+//VARIABLES
+
+//variable asignada (let ó const) + nombreVarible + operador de asignacion +  contenido
+
+alert ("Bienvenido a Zasa Arte")
+
+let saludo = "Bienvenido a Zasa Arte"
+console.log(saludo)
+
+let tipoProducto = prompt("Que producto buscas?")
+const anio = 2023
+
+/* let nombreProducto = (tipoProducto)
+console.log(nombreProducto) */
+
+let tecnicaArtistica = prompt("Que tecnica artistica buscas?")
+
+/* let tecnicaSeleccionada = (tecnicaArtistica)
+console.log(tecnicaSeleccionada) */
+
+let producto = "El producto que esta buscando es:"
+console.log(producto)
+
+
+let nombreCompleto = `${tipoProducto} en ${tecnicaArtistica}`
+console.log(nombreCompleto)
+
+
+/* let nombreProducto = "Cuadro:"
+const anio = 2023
+
+let nombreCuadro = " Sitiendo"
+
+let nombreCompleto = nombreProducto + nombreCuadro
+console.log(nombreCompleto)  */
+
+//Ejemplo 2
+//Calcular edad
+
+/* let anioNacimiento = 1987
+
+let edad = anio - anioNacimiento
+console.log(edad)  */
+
+
+//let nombreCuadro = " Sitiendo"
+
+/* let nombreCompleto = `${nombreProducto} Sintiendo`
+console.log(nombreCompleto)
+ */
+//Ejemplo calcular edad
+
+/* let anioNacimiento = 1987
+
+let edad = anio - anioNacimiento
+console.log(edad)  */
+
+// 
+
+/* let anioNacimiento = prompt("Diga su año de nacimiento") //"1987"
+
+parseInt(anioNacimiento) //Nan */
+
+//CONDICIONALES
+
+/* let edad = parseInt(prompt("Decime tu edad"))
+
+if (edad >= 18) {
+
+    alert("Bienvenido")
+} else {
+    alert("Debes estar en compañia de un adulto")
+    
+} */
+
