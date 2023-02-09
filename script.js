e223541150f20f586543392482fbf6b22c956f3a
zasa87 <zasa.arte@gmail.com>
2023-02-08 20:44:15 -0500

    actualizado hasta funciones

--- script.js
+++ script.js
@@ -2,30 +2,67 @@
 
 //variable asignada (let ó const) + nombreVarible + operador de asignacion +  contenido
 
-alert ("Bienvenido a Zasa Arte")
+function saludoGenerico() {
+    alert("Bienvenido a Zasa Arte")
+}
+
+saludoGenerico()
+
+let nombreUsuario = prompt("Decime tu nombre")
+
+function saludoEspecifico(nombre) {
+    
+    alert(`Hola ${nombre}`)
+}
+
 
 //selecionar producto
 
-let saludo = "Bienvenido a Zasa Arte"
-console.log(saludo)
-
-let tipoProducto = prompt("Que producto buscas?")
+let tipoProducto = prompt("Que producto de arte busca?")
 const anio = 2023
-
-let tecnicaArtistica = prompt("Que tecnica artistica buscas?")
-
-let nombreCuadro = prompt("Escriba el nombre del cuadro")
 
 let producto = "El producto que esta buscando es:"
 console.log(producto)
 
-let nombreCompleto = `${tipoProducto} en ${tecnicaArtistica} ${nombreCuadro}`
+let nombreCompleto = `${tipoProducto}`
 console.log(nombreCompleto)
 
-//CONDICIONAL
+//CONDICIONALES
+//Se ejecuta una sola vez el codigo
 
-for (let i = 0; i < 5; i++){
-    let nombreReserva = prompt("Decime tu nombre")
-    alert(`La mesa Nro ${i}, está reservada a nombre de ${nombreReserva}`)
-}  
+ let  nombreProductos= prompt("Escriba el numero de la tecnica que quiera elegir: 1)Tecnica Artistica, 2)Ilustración 3)Impresión")
+
+if (nombreProductos == 1) {
+    alert("Ha elegido cuadro")
+} else if(nombreProductos == 2){
+    alert("Ha elegido Ilustración")
+} else if(nombreProductos == 3){  
+    alert("Ha elegido Impresión")
+}
+
+//CICLOS
+//Se repite una determinada numero de veces
+// i significa iterando, es decir que, hace que se repita un numero determinado de veces
+//i++ es igualo que escribir  i = i + 1
+//Por conteo - FOR
+
+for (let i = 0; i < 2; i++){
+    let dimensionesProducto = prompt("Escriba una dimensión en centimetros")
+    alert(`el cuadro Nro ${i},sera de ${dimensionesProducto}`)
+} 
+alert("Su pedido quedo registrado") 
+
+
+ //POR CONDICION - while
+
+ let numerocelular = parseInt(prompt("Escribe tu numero de celular"))
+while (isNaN(numerocelular)) {
+    alert("Ingreso un valor no númerico, por favor corrija")
+    numerocelular = parseInt(prompt("Escribe tu numero de celula"))
+}
+alert("Nos comunicaremos con usted")
+
+//Is Not a Number -- Devuelve true cuando el valor analizado sea NaN
+
+//FUNCIONES
 
